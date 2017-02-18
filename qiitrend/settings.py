a4142de0d57d7ded17be617ca3443f8d8eaebfcc
tag1216 <taguti1216@gmail.com>
2017-02-18 15:15:35 +0900

    honeypotの導入

--- qiitrend/settings.py
+++ qiitrend/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'webpack_loader',
+    'admin_honeypot',
     'core',
 ]
 
--- qiitrend/urls.py
+++ qiitrend/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+import os
+
 from django.conf.urls import url, include
 from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
+    url(r'^{}'.format(os.environ.get('ADMIN_URL', 'adminsite/')), admin.site.urls),
     url(r'', include('core.urls')),
 ]

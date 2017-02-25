0f3249e87ae879072630bd935c327ac12561107a
tag1216 <taguti1216@gmail.com>
2017-02-25 20:16:20 +0900

    productionビルドの修正

--- qiitrend/settings.py
+++ qiitrend/settings.py
@@ -167,15 +167,10 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 WEBPACK_LOADER = {
     'DEFAULT': {
-        'BUNDLE_DIR_NAME': 'bundles/',
-        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
-    }
-}
-if not DEBUG:
-    WEBPACK_LOADER['DEFAULT'].update({
         'BUNDLE_DIR_NAME': 'dist/',
         'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats-prod.json')
-    })
+    }
+}
 
 
 ########################################
--- qiitrend/settings_dev.py
+++ qiitrend/settings_dev.py
@@ -1,3 +1,10 @@
 from .settings import *
 
 DEBUG = True
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'BUNDLE_DIR_NAME': 'bundles/',
+        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
+    }
+}

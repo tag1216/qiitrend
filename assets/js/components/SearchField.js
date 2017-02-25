fdd512d89fda2f4c1fd298d1a2e66bd0781d436a
tag1216 <taguti1216@gmail.com>
2017-02-25 11:50:38 +0900

    トップページの検索ボックスのスタイル設定

---
+++ assets/js/components/SearchField.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import { TextField, FlatButton } from "material-ui";
+import muiThemeable from 'material-ui/styles/muiThemeable';
+import SearchAction from 'material-ui/svg-icons/action/search';
+
+
+class SearchField extends Component {
+
+  state = {
+    value: ""
+  };
+
+  handleChange(event, value) {
+    this.setState({value});
+  }
+
+  handleKeyDown(event) {
+    if (event.keyCode == 13) {
+      const value = this.state.value;
+      this.props.onSubmit(value);
+    }
+  }
+
+  handleClick() {
+    const value = this.state.value;
+    this.props.onSubmit(value);
+  }
+
+  render() {
+    const icon = (
+      <SearchAction style={{marginBottom: 6}}
+                    color={this.props.muiTheme.palette.alternateTextColor}/>
+    );
+    return (
+      <div style={{position: "relative"}}>
+        <TextField name="q"
+                   value={this.state.value}
+                   underlineShow={false}
+                   autoFocus={true}
+                   onChange={this.handleChange.bind(this)}
+                   onKeyDown={this.handleKeyDown.bind(this)}
+                   style={{
+                     width: 300,
+                     height: 32,
+                     marginTop: 5,
+                     borderTopLeftRadius: 16,
+                     borderBottomLeftRadius: 16,
+                     backgroundColor: "#fff",
+                     overflow: "hidden",
+                   }}
+                   inputStyle={{ marginLeft: 24}}
+
+        />
+        <FlatButton icon={icon}
+                    onClick={this.handleClick.bind(this)}
+                    backgroundColor={this.props.muiTheme.palette.accent1Color}
+                    style={{
+                      minWidth: 48,
+                      height: 32,
+                      position: "absolute",
+                      top: 0,
+                      bottom: 0,
+                      margin: "auto",
+                      borderTopRightRadius: 16,
+                      borderBottomRightRadius: 16,
+                    }}
+        />
+      </div>
+    );
+  }
+}
+
+export default muiThemeable()(SearchField);
--- assets/js/components/pages/Home.js
+++ assets/js/components/pages/Home.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import { Paper, TextField } from "material-ui";
+import { Paper } from "material-ui";
 import muiThemeable from 'material-ui/styles/muiThemeable';
 
-import QueryField from "../QueryField";
+import SearchField from "../SearchField";
+
 
 class Home extends Component {
 
@@ -13,16 +14,12 @@ class Home extends Component {
   render() {
 
     const styles = {
-      paper1: {
+      paper: {
         width: "100%",
         paddingTop: 16,
         paddingBottom: 64,
         backgroundColor: this.props.muiTheme.palette.primary1Color,
         color: this.props.muiTheme.palette.alternateTextColor,
-      },
-      paper2: {
-        width: "100%",
-        paddingTop: 16,
       },
       h1: {
         fontSize: 64,
@@ -35,42 +32,25 @@ class Home extends Component {
         textAlign: 'center',
       },
       search: {
-        color: this.props.muiTheme.palette.alternateTextColor,
-      },
-      searchIcon: {
-        marginRight: 16,
-        color: this.props.muiTheme.palette.alternateTextColor,
-      }
-    };
-
-    const searchFieldProps = {
-      name: "q",
-      autoFocus: true,
-      inputStyle: {
-        color: this.props.muiTheme.palette.alternateTextColor,
-      },
-      underlineStyle: {
-        color: this.props.muiTheme.palette.primary2Color,
-      },
-      underlineFocusStyle: {
-        color: this.props.muiTheme.palette.alternateTextColor,
+        marginTop: 64,
       },
     };
 
     return (
       <div>
-        <Paper style={styles.paper1}>
+        <Paper style={styles.paper}>
           <div style={styles.tagline}>
             <h1 style={styles.h1}>QiiTrend</h1>
             <h2 style={styles.h2}>QiiTrendは、Qiitaの投稿件数の推移を時系列チャートで表示するサービスです。</h2>
-            <div>チャートに表示したい投稿の検索クエリーを入力してください。</div>
-            <QueryField showIcon={true}
-                        iconProps={{style: styles.searchIcon}}
-                        textFieldProps={searchFieldProps}
-                        onSubmit={this.props.onAddQuery}
-            />
-            <div>例) 「tag:python」 「tag:go or tag:golang」 「scocks:>=100」 </div>
-            <div>検索方法は、Qiitaの検索と同じです。詳細はこちらを参照してください。</div>
+            <div style={styles.search}>
+              <div>チャートに表示したい投稿の検索条件を入力してください。</div>
+              <SearchField onSubmit={this.props.onAddQuery}/>
+              <div>例) 「tag:python」 「tag:go or tag:golang」 「scocks:>=100」 </div>
+              <div>
+                検索方法は <a href="http://qiita.com/search" target="_blank">Qiita</a> と同じです。
+                詳細は <a href="http://help.qiita.com/ja/articles/qiita-search-options" target="_blank">こちら</a> を参照してください。
+              </div>
+            </div>
           </div>
         </Paper>
       </div>

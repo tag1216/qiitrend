cde24a40d028d6962b2a366ffe9745ccc0c5b40a
tag1216 <taguti1216@gmail.com>
2017-03-04 17:42:21 +0900

    紹介記事と例へのリンクを追加

--- assets/js/components/Header.js
+++ assets/js/components/Header.js
@@ -1,53 +1,75 @@
-import React, { propTypes } from "react";
-import { Component } from "react";
-import { AppBar, FlatButton, IconMenu, IconButton, MenuItem, Avatar } from "material-ui";
-import { OutboundLink } from "react-ga";
+import React, {propTypes, Component} from "react";
+import {AppBar, IconMenu, IconButton, MenuItem, Avatar} from "material-ui";
+import MoreVertIcon from "material-ui/svg-icons/navigation/more-vert";
+import muiThemeable from "material-ui/styles/muiThemeable";
+import {OutboundLink} from "react-ga";
 
 
-class Login extends Component {
-  static muiName = 'FlatButton';
-
+class Profile extends Component {
   render() {
     return (
-      <FlatButton {...this.props} label="Qiitaアカウントでログイン" />
+      <Avatar size={24} src={this.props.user.profile_image_url} />
     );
   }
 }
 
-class Logged extends Component {
-  static muiName = 'IconMenu';
-
+class RightMenu extends Component {
+  static muiName = "IconMenu";
   render() {
+    const iconButton = <IconButton><MoreVertIcon/></IconButton>;
+    const sendFormLink = (
+      <OutboundLink eventLabel="send form"
+                    to="https://ssl.form-mailer.jp/fms/d6036533496614"
+                    target="_blank"
+      />
+    );
+    const aboutLink = (
+      <OutboundLink eventLabel="about"
+                    to="http://qiita.com/tag1216/items/e65496b932ca6790268b"
+                    target="_blank"
+      />
+    );
+    const examplesLink = (
+      <OutboundLink eventLabel="examples"
+                    to="http://qiita.com/tag1216/items/ae2f197b211213410602"
+                    target="_blank"
+      />
+    );
+
+    const iconMenuProps = {
+      iconStyle: this.props.iconStyle,
+      onItemTouchTap: this.props.onItemTouchTap,
+    };
+
     return (
-      <IconMenu
-        {...this.props}
-        iconButtonElement={
-          <IconButton><Avatar size={24} src={this.props.user.profile_image_url} /></IconButton>
+      <IconMenu {...iconMenuProps}
+                iconButtonElement={iconButton}>
+        <MenuItem primaryText="QiiTrendについて" containerElement={aboutLink}/>
+        <MenuItem primaryText="チャートの例" containerElement={examplesLink}/>
+        <MenuItem primaryText="ご意見・バグ報告" containerElement={sendFormLink}/>
+        {this.props.loggedIn
+          ? <MenuItem value="logout" primaryText="ログアウト" />
+          : <MenuItem value="login" primaryText="Qiitaアカウントでログイン" />
         }
-        targetOrigin={{horizontal: 'right', vertical: 'top'}}
-        anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-      >
-        <MenuItem containerElement={
-          <OutboundLink eventLabel="send form"
-                        to="https://ssl.form-mailer.jp/fms/d6036533496614"
-                        target="_blank"
-        />}
-                  primaryText="ご意見・バグ報告" />
-        <MenuItem value="logout" primaryText="ログアウト" />
       </IconMenu>
     );
   }
 }
 
-export default class Header extends Component {
+class Header extends Component {
 
   static contextTypes = {
     router: React.PropTypes.object.isRequired,
   };
 
   handleItemTouchTap(event, child) {
-    if (child.props.value === "logout") {
-      this.props.onLogout();
+    switch (child.props.value) {
+      case "logout":
+        this.props.onLogout();
+        break;
+      case "login":
+        this.props.onLogin();
+        break;
     }
   }
 
@@ -61,13 +83,19 @@ export default class Header extends Component {
       },
     };
 
+    const title = (
+      <span style={styles.title}
+            onClick={() => router.push("/")}>
+        QiiTrend
+      </span>
+    );
+
     const appBarProps = {
-      title: (
-        <span style={styles.title}
-              onClick={() => router.push("/")}>
-          QiiTrend
-        </span>
-      )
+      showMenuIconButton: false,
+      title,
+      iconStyleRight: {
+
+      },
     };
 
     if (isHome) {
@@ -77,17 +105,35 @@ export default class Header extends Component {
       });
     }
 
+    const palette = this.props.muiTheme.palette;
+
+    const profileProps = {
+      ...this.props,
+    };
+
+    const rightMenuProps = {
+      ...this.props,
+      user: this.props.user,
+      iconStyle: {
+        color: palette.alternateTextColor,
+      },
+      onItemTouchTap: this.handleItemTouchTap.bind(this),
+    };
+
     return (
       <header>
         <AppBar {...appBarProps}
-                showMenuIconButton={false}
                 iconElementRight={
-                  this.props.loggedIn
-                    ? <Logged onItemTouchTap={this.handleItemTouchTap.bind(this)}
-                              user={this.props.user}/>
-                    : <Login onClick={this.props.onLogin} />}
-        />
+                  <div>
+                    {this.props.loggedIn ? <Profile {...profileProps}/>: ""}
+                    <RightMenu {...rightMenuProps}/>
+                  </div>
+                }
+        >
+        </AppBar>
       </header>
     );
   }
 }
+
+export default muiThemeable()(Header);
